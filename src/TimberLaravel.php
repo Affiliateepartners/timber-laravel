577323ee2eece6b6f2f7a5f69a4a5ea1be8e3228
Michal Kluczek <michal.kluczek@liteweb.io>
2018-02-22 16:51:03 +0100

    Hotfix

--- src/TimberLaravel.php
+++ src/TimberLaravel.php
@@ -205,7 +205,7 @@ class TimberLaravel
 
         // There must be a backtrace...
         if(!$this->context['TAPI_event']['error']['backtrace'])
-            $this->context['TAPI_event']['error']['backtrace'] = ['file' => '_no_trace_', 'line' => 1, 'function' => '_no_trace_'];
+            $this->context['TAPI_event']['error']['backtrace'] = [['file' => '_no_trace_', 'line' => 1, 'function' => '_no_trace_']];
 
         \Log::critical($message, $this->context);
     }
