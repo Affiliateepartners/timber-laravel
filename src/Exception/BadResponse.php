78b1fabe3f34617296cc704115be11f4632853ad
Michal Kluczek <michal.kluczek@liteweb.io>
2018-04-11 17:52:20 +0200

    Added possibility to enter custom exception message

--- src/Exception/BadResponse.php
+++ src/Exception/BadResponse.php
@@ -5,11 +5,11 @@ class BadResponse extends \Exception
 {
 	private $response;
 
-	public function __construct(\Psr\Http\Message\ResponseInterface $response)
+	public function __construct(\Psr\Http\Message\ResponseInterface $response, ?string $message = null)
 	{
 		$this->response = $response;
 
-		parent::__construct('Invalid response code');
+		parent::__construct($message ?? 'Invalid response code');
 	}
 
 	public function getResponse(): \Psr\Http\Message\ResponseInterface
